cca73f27044240df503f4c0b7db608218d8da7fc
malyz <mattuy001@gmail.com>
2020-08-27 15:03:40 +1200

    Updated content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="PokeVue" />
+    <div>Pokemon cards</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Title from '@/components/Title'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>

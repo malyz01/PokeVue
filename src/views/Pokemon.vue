3886103d184b34f98a79745149653dee3167c85d
malyz <mattuy001@gmail.com>
2020-08-27 20:23:11 +1200

    Added app state in pokemon component

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="container">
     <Title title="My Pokemon" />
+    <div v-for="pokemon in allPokemon" :key="pokemon.name">
+      {{ pokemon.name }}
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import Title from '@/components/Title'
 
 export default {
   name: 'Pokemon',
   components: {
     Title
+  },
+  methods: {
+    ...mapActions(['fetchPokemons'])
+  },
+  computed: {
+    ...mapGetters(['allPokemon'])
+  },
+  created() {
+    this.fetchPokemons()
   }
 }
 </script>

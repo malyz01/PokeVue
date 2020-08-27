bee6622917b74d0be0a11c9868c3b5f43a2631be
malyz <mattuy001@gmail.com>
2020-08-27 15:02:44 +1200

    Added title component

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped></style>

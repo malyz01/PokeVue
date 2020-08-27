8d411796ff159acbccd2eb87889a5fd2d2c76cfa
malyz <mattuy001@gmail.com>
2020-08-27 15:03:03 +1200

    Added Pokemon view/page

---
+++ src/views/Pokemon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Title title="My Pokemon" />
+  </div>
+</template>
+
+<script>
+import Title from '@/components/Title'
+
+export default {
+  name: 'Pokemon',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 1em;
+}
+</style>

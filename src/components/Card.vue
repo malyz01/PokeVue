2dbed65f960f000c4e0e5064d2b91fdd3981a62c
unknown <mattuy001@gmail.com>
2020-09-16 22:20:00 +1200

    Created card component

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-card class="card">
+    <b-card-header class="header">
+      <h4 class="mb-0">{{pokemon.name}}</h4>
+    </b-card-header>
+    <img class="image" src="@/assets/images/pokeball.png" />
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    pokemon: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  max-width: 300px;
+  margin: 2rem 1rem;
+  cursor: pointer;
+}
+
+.header {
+  text-transform: capitalize;
+  background-color: #ea0b00;
+  color: azure;
+}
+
+.image {
+  max-width: 300px;
+  width: 100%;
+}
+</style>

03b77eff6e54c477ac1abbfcf1f9f2b2e7885757
malyz <mattuy001@gmail.com>
2020-09-16 17:58:48 +1200

    Cleaned up App component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/pokemon">My Pokemon</router-link>
-    </div>
+    <AppHeader />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from './components/layout/AppHeader'
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,19 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-  background-color: #adfcd8;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>

cd932e4f156d1657012990c6dc696fc9a98c1da4
malyz <mattuy001@gmail.com>
2020-09-16 17:59:00 +1200

    Created Test Component

---
+++ src/components/Primary.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Sub @emit-me="alertMe" />
+    <div>{{ txt }}</div>
+  </div>
+</template>
+
+<script>
+import Sub from './Sub'
+
+export default {
+  name: 'Primary',
+  data: function() {
+    return {
+      txt: ''
+    }
+  },
+  components: {
+    Sub
+  },
+  methods: {
+    alertMe(prop) {
+      console.log(prop)
+      this.txt = prop
+    }
+  }
+}
+</script>

fb0f007054eac5c50f6eed0e7f0885c16dc08392
malyz <mattuy001@gmail.com>
2020-09-16 18:00:08 +1200

    Added header component

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pokemon">My Pokemon</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+#nav a {
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+}
+
+#nav a.router-link-exact-active {
+  color: #e60b00;
+}
+</style>

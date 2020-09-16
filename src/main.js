2bb843bf400f66dfcc6c6f8b1b63fe45fe732fcb
malyz <mattuy001@gmail.com>
2020-09-16 20:23:26 +1200

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/bootstrap-vue'
 
 Vue.config.productionTip = false
 

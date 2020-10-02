ebc7c122f987c0edd687064cbef3f76ae0cb8bde
malyz <mattuy001@gmail.com>
2020-10-02 14:54:01 +1300

    Created home card component

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="main">This is a card</main>
+</template>
+
+<script>
+export default {
+  name: 'HomeCard'
+}
+</script>
+
+<style scoped>
+.main {
+  background-color: beige;
+  color: black;
+  height: 100%;
+  border-radius: 25px;
+}
+</style>

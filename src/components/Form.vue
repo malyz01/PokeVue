4cf530066385f98d4db84cb01f31874cd5355213
unknown <mattuy001@gmail.com>
2020-10-02 23:24:34 +1300

    Created form

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="main">
+    <form>
+      <input v-model="name" type="text" />
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "myForm",
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  max-width: 200px;
+}
+</style>
